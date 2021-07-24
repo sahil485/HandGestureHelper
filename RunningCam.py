e2c48ae76d170f1ea8bc5ed99ced14da09942816
sillyhill-c <sahil.chatiwala@gmail.com>
2021-07-23 23:35:02 -0400

    Implements four possible states - thumb up, thumb down, forward, and none (empty)

--- RunningCam.py
+++ RunningCam.py
@@ -1,20 +1,24 @@
 import cv2
 import tensorflow as tf
 import numpy as np
-from pynput.mouse import Controller
-
-mouse = Controller()
-network = tf.keras.models.load_model("gestures_modelv4")
+import time
+from pynput.mouse import Controller as mouseController
+from pynput.keyboard import Key, Controller
+mouse = mouseController()
+key = Controller()
+network = tf.keras.models.load_model("gestures_modelv6")
 
 def classification(image):
     global network
     prediction = network.predict(image)
-    if prediction[0][1]+prediction[0][2]<0.95:
+    if prediction[0][1]+prediction[0][2]+ prediction[0][3]<0.95 and prediction[0][0]>.20:
         return 0
-    elif max(prediction[0]) == prediction[0][1]:
-        return 1
-    else:
+    elif max(prediction[0]) == prediction[0][1] and prediction[0][1]>prediction[0][2]+0.25:
+        return 3
+    elif max(prediction[0]) == prediction[0][3]:
         return 2
+    else:
+        return 1
 
 def none(predict): True if predict[0][1]+predict[0][2]<0.95 else False
 
@@ -26,6 +30,7 @@ def thumbDown(predict): True if max(predict[0]) == predict[0][2] else False
 
 video_feed = cv2.VideoCapture(0)
 
+cTime = 0
 
 while True:
     success, frame = video_feed.read()
@@ -36,7 +41,7 @@ while True:
 
 
     cv2.imshow("win", monoFrame)
-    cv2.waitKey(50)
+    cv2.waitKey(25)
 
     monoFrame = cv2.resize(monoFrame, (40, 30))
     monoFrame = np.reshape(monoFrame, [1, 40, 30, 1])
@@ -49,5 +54,11 @@ while True:
     elif decision == 2:
         mouse.scroll(0,-0.5)
         print("thumbDown")
+    elif decision == 3:
+        key.press(Key.ctrl)
+        key.press(Key.tab)
+        key.release(Key.ctrl)
+        key.release(Key.tab)
+        print("forward")
     else:
         print("none")
